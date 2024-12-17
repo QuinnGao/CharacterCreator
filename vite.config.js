b6d4d1d94da651c5a2ca36aa49364890ea28f581
Quinn Gao <gao.kings@gmail.com>
2024-12-17 13:42:10 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   publicPath: "/CharacterCreator/",
-  base: "./",
+  base: "/CharacterCreator",
   plugins: [react()],
 })
