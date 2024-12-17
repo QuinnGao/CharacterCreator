24bc0a542da1536a3ff2ec00108dee6fdfcc1de0
Quinn Gao <gao.kings@gmail.com>
2024-12-17 15:06:17 +0800

    Update Create.jsx

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -38,7 +38,7 @@ function Create({fetchNewModel}) {
         name: t('classes.beastPainter.name'),
         image: import.meta.env.VITE_PUBLIC_PATH + "/assets/media/disabled.png",
         description: t('classes.beastPainter.description'),
-        icon: "/assets/icons/class-beast-painter.svg",
+        icon: import.meta.env.VITE_PUBLIC_PATH + "/assets/icons/class-beast-painter.svg",
         disabled: true,
         templateIndex:2
       },
@@ -46,7 +46,7 @@ function Create({fetchNewModel}) {
         name: t('classes.engineer.name'),
         image: import.meta.env.VITE_PUBLIC_PATH + "/assets/media/disabled.png",
         description: t('classes.engineer.description'),
-        icon: "/assets/icons/class-engineer.svg",
+        icon: import.meta.env.VITE_PUBLIC_PATH + "/assets/icons/class-engineer.svg",
         disabled: true,
         templateIndex:3
       },
@@ -54,7 +54,7 @@ function Create({fetchNewModel}) {
         name: t('classes.dropHunter.name'),
         image: import.meta.env.VITE_PUBLIC_PATH + "/assets/media/DropHunter.png",
         description: t('classes.dropHunter.description'),
-        icon: "/assets/icons/class-drop-hunter.svg",
+        icon: import.meta.env.VITE_PUBLIC_PATH + "/assets/icons/class-drop-hunter.svg",
         disabled: false,
         templateIndex:0
       },
@@ -62,7 +62,7 @@ function Create({fetchNewModel}) {
         name: t('classes.theDegen.name'),
         image: import.meta.env.VITE_PUBLIC_PATH + "/assets/media/degens.gif",
         description: t('classes.theDegen.description'),
-        icon: "/assets/icons/class-the-degen.svg",
+        icon: import.meta.env.VITE_PUBLIC_PATH + "/assets/icons/class-the-degen.svg",
         disabled: true,
         templateIndex:6
       },
@@ -70,7 +70,7 @@ function Create({fetchNewModel}) {
         name: t('classes.neuralHacker.name'),
         image: import.meta.env.VITE_PUBLIC_PATH + "/assets/media/NeuralHacker.png",
         description: t('classes.neuralHacker.description'),
-        icon: "/assets/icons/class-neural-hacker.svg",
+        icon: import.meta.env.VITE_PUBLIC_PATH + "/assets/icons/class-neural-hacker.svg",
         disabled: false,
         templateIndex:1
       },
@@ -78,7 +78,7 @@ function Create({fetchNewModel}) {
         name: t('classes.liskWitch.name'),
         image: import.meta.env.VITE_PUBLIC_PATH + "/assets/media/disabled.png",
         description: t('classes.liskWitch.description'),
-        icon: "/assets/icons/class-lisk-witch.svg",
+        icon: import.meta.env.VITE_PUBLIC_PATH + "/assets/icons/class-lisk-witch.svg",
         disabled: true,
         templateIndex:4
       },
@@ -86,7 +86,7 @@ function Create({fetchNewModel}) {
         name: t('classes.bruiser.name'),
         image: import.meta.env.VITE_PUBLIC_PATH + "/assets/media/disabled.png",
         description: t('classes.bruiser.description'),
-        icon: "/assets/icons/class-bruiser.svg",
+        icon: import.meta.env.VITE_PUBLIC_PATH + "/assets/icons/class-bruiser.svg",
         disabled: true,
         templateIndex:5
       },
